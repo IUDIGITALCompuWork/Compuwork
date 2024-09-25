396a522511a5dfac862189f79855f0692fb7f2e9
M4rlon.Mora <marlon.mora@est.iudigital.edu.co>
2024-09-24 22:05:39 -0500

    Creacion de la clase de Main

--- src/Clases/Empleado.java
+++ src/Clases/Empleado.java
@@ -9,20 +9,14 @@ public class Empleado {
     private String apellido;
     private String fechaContratacion;
     private String tipoContrato; // "permanente" o "temporal"
-    private String email;
-    private String telefono;
-    private String direccion;
     private Departamento departamento;
 
-    public Empleado(String id, String nombre, String apellido, String fechaContratacion, String tipoContrato, String email, String telefono, String direccion) {
+    public Empleado(String id, String nombre, String apellido, String fechaContratacion, String tipoContrato) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.fechaContratacion = fechaContratacion;
         this.tipoContrato = tipoContrato;
-        this.email = email;
-        this.telefono = telefono;
-        this.direccion = direccion;
     }
 
     // Métodos getter y setter
@@ -31,9 +25,6 @@ public class Empleado {
     public String getApellido() { return apellido; }
     public String getFechaContratacion() { return fechaContratacion; }
     public String getTipoContrato() { return tipoContrato; }
-    public String getEmail() { return email; }
-    public String getTelefono() { return telefono; }
-    public String getDireccion() { return direccion; }
 
     public void setDepartamento(Departamento departamento) {
         this.departamento = departamento;
@@ -43,3 +34,4 @@ public class Empleado {
         return "Reporte de Desempeño de " + nombre + " " + apellido;
     }
 }
+
